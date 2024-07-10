becc66493222a06af32e97711815c6b5244133a0
Vitor Augusto <vitor.silva@trescon.com.br>
2024-07-09 23:35:01 -0300

    first Commit

---
+++ loja/main/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from.models import Category,Marca, Color, Size, Product, ProductAttribute
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Marca)
+admin.site.register(Color)
+admin.site.register(Size)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'marca', 'color', 'size', 'status')
+    list_editable = ('status',)
+admin.site.register(Product,ProductAdmin)
+
+class ProductAttributeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'price', 'color', 'size')
+
+admin.site.register(ProductAttribute,ProductAttributeAdmin)    
+
